ffdb7b144d66e74db9c82b2535d6da3f1a12af7d
snowway <snowway.xue@gmail.com>
2014-09-17 11:46:27 +0800

    增加public controller

---
+++ platform/lib/website/controller/public.js
@@ -0,0 +1,34 @@
+var controller = require("./controller")
+    , _ = require("../../foundation/core")._
+    , logger = _.logger("controller.public");
+
+/**
+ * 控制台界面
+ */
+controller.get("/", function (req, res) {
+    res.render("dashboard");
+});
+
+
+/**
+ * 登录界面
+ */
+controller.get("/login",function(req,res,next){
+    res.render("login",{layout:'default'});
+});
+
+
+/**
+ * 登录处理
+ */
+controller.post("/login",function(req,res,next){
+    res.redirect("/");
+});
+
+
+/**
+ * 注销
+ */
+controller.get("/logout",function(req,res,next){
+    res.redirect("/login");
+});
